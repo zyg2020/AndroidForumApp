33eb5c6a7c16eafcf177cb64734f24f44b6e7e8d
zyg2020 <zhuyange2018@gmail.com>
2021-10-26 11:43:02 -0500

    Add InitialLoginActivity. Complete layout of it.

--- app/src/main/java/com/yangezhu/forumproject/InitialLoginActivity.java
+++ app/src/main/java/com/yangezhu/forumproject/InitialLoginActivity.java
@@ -4,15 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
-public class InitialActivity extends AppCompatActivity {
+public class InitialLoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_initial);
+        setContentView(R.layout.activity_initial_login);
 
         Button btn = findViewById(R.id.btn_to_main);
 
