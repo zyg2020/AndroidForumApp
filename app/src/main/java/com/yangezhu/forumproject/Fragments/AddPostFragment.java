8e714d31594c2600291b0ac9783022b85ce3dd56
zyg2020 <zhuyange2018@gmail.com>
2021-10-30 19:23:46 -0500

    Add post's fields and enable users to select multiple images

--- app/src/main/java/com/yangezhu/forumproject/Fragments/AddPostFragment.java
+++ app/src/main/java/com/yangezhu/forumproject/Fragments/AddPostFragment.java
@@ -1,20 +1,52 @@
 package com.yangezhu.forumproject.Fragments;
 
+import android.app.Activity;
+import android.content.ClipData;
+import android.content.Intent;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.yangezhu.forumproject.R;
+import com.yangezhu.forumproject.adapter.NewsAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.transform.Result;
 
 /**
  * A simple {@link Fragment} subclass.
  * create an instance of this fragment.
  */
 public class AddPostFragment extends Fragment {
+
+    private Spinner category_spinner;
+    private Button btn_upload_images;
+    private TextView txt_display_upload_images;
+
+    ArrayList<Uri> mArrayUri = new ArrayList<Uri>();
+
+    String[] categories_list = {"Used Items", "Marketing", "Rent", "Used Cars"};
+    private String selected_category;
+    final int PICK_IMAGE_MULTIPLE = 1;
+    String imageEncoded;
+    List<String> imagesEncodedList;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -26,6 +58,101 @@ public class AddPostFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_post, container, false);
+        View view = inflater.inflate(R.layout.fragment_add_post, container, false);
+
+        category_spinner = (Spinner) view.findViewById(R.id.category);
+        category_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                // On selecting a spinner item
+                String item = adapterView.getItemAtPosition(i).toString();
+                selected_category = item;
+                // Showing selected spinner item
+                Toast.makeText(adapterView.getContext(), "Selected: " + item, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        List<String> categories_list = new ArrayList<String>();
+        categories_list.add("Used Items");
+        categories_list.add("Marketing");
+        categories_list.add("Rent");
+        categories_list.add("Used Cars");
+
+        ArrayAdapter<String> categories_adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_item, categories_list);
+
+        // Drop down layout style - list view with radio button
+        categories_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        category_spinner.setAdapter(categories_adapter);
+
+        btn_upload_images = (Button) view.findViewById(R.id.btn_upload_images);
+        btn_upload_images.setOnClickListener(view1 -> {
+            Intent intent = new Intent();
+            intent.setType("image/*");
+            intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            startActivityForResult(Intent.createChooser(intent,"Select Picture"), PICK_IMAGE_MULTIPLE);
+        });
+
+        txt_display_upload_images = (TextView) view.findViewById(R.id.txt_display_upload_images);
+
+        return view;
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        try {
+            // When an Image is picked
+            if (requestCode == PICK_IMAGE_MULTIPLE && resultCode == Activity.RESULT_OK && null != data) {
+                // Get the Image from data
+
+                String[] filePathColumn = { MediaStore.Images.Media.DATA };
+                imagesEncodedList = new ArrayList<String>();
+                if(data.getData()!=null){
+                    String imagePath = data.getData().getPath();
+                    Log.d("YZHU_IMAGE_SELECT", "One image --> " + imagePath);
+                    Uri mImageUri=data.getData();
+                    mArrayUri.add(mImageUri);
+
+                    String display_text = "";
+                    for (int i = 0; i < mArrayUri.size(); i++) {
+
+                        display_text += mArrayUri.get(i).toString() + "\n";
+                        Log.d("YZHU_IMAGE_SELECT", "Multiple images --> " + imagePath);
+                    }
+                    txt_display_upload_images.setText(display_text);
+
+                } else {
+                    if (data.getClipData() != null) {
+                        ClipData mClipData = data.getClipData();
+                        ArrayList<Uri> mArrayUri = new ArrayList<Uri>();
+
+                        int count = mClipData.getItemCount();
+                        for (int i = 0; i < count; i++) {
+
+                            ClipData.Item item = mClipData.getItemAt(i);
+                            Uri uri = item.getUri();
+                            mArrayUri.add(uri);
+
+                            String imagePath = item.toString();
+                            Log.d("YZHU_IMAGE_SELECT", "Multiple images --> " + imagePath);
+                        }
+                        Log.v("LOG_TAG", "Selected Images" + mArrayUri.size());
+                    }
+                }
+            } else {
+                Toast.makeText(getContext(), "You haven't picked Image", Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception e) {
+            Toast.makeText(getContext(), "Something went wrong", Toast.LENGTH_LONG).show();
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
 }
